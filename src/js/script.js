796d1dedb83c262356505afaa78db52414e0f24e
Kaike Sousa <kaike.cyw@gmail.com>
2025-05-28 00:01:46 -0300

    Adicionando nav-bar pronto.

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// INICIO HEADER
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "src/imgs/menu.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "src/imgs/exit.svg";
+    }
+}
+// FIM DO HEADER
+
+
+
